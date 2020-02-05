571e609980e79527118d66a705a0d9e8717b6ccc
htunhtunhtet <ryanhhh91@gmail.com>
2020-02-05 18:03:22 +0800

    banner module

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import banner from  './images/banner.jpg';
+import logo from './images/logo.png';
+import { Menu, Layout,Carousel  } from 'antd';
+const {  Content, Footer } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => (
+    <div>
+        <Layout className="layout">
+            <Menu
+                mode="horizontal"
+                defaultSelectedKeys={['4']}
+                style={{
+                    lineHeight: '64px',
+                    backgroundColor: 'transparent',
+                    position: "absolute",
+                    zIndex: 1
+
+                }}
+            >
+
+                <Menu.Item className={"logo-container"}>
+                    <img src={logo} alt="logo" style={{
+                        width: 40,
+                        borderBottom: 0
+                    }}/>
+                </Menu.Item>
+            </Menu>
+
+            <Carousel>
+                <div className={'banner-container'}>
+                    <div className={"banner-overlay"} />
+                    <img src={banner}  alt="banner" />
+                </div>
+            </Carousel>
+
+            <Content style={{ padding: '0 50px' }}>
+                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>,
     </div>
-  );
-}
+);
 
 export default App;
